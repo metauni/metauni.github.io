3280baea4ede2c19c695259f3862a03e862b5508
EthanCurtiss <ethanhcurtiss@gmail.com>
2022-06-11 19:28:25 -0400

    Validate that seminar times are sensical and do not overlap

--- schedule/validate-schedule.py
+++ schedule/validate-schedule.py
@@ -17,6 +17,34 @@ def validate_seminars(schedule):
             if type(value) != str: # Currently all properties are string. This could change. /shrug
                 raise Exception(f"\"key\" property for seminar \"{name}\" must be a string, got {type(value).__name__}")
 
+# Validates that the seminar times do not overlap
+def validate_seminar_times(seminars):
+    times = []
+    for seminar in seminars:
+        name = next(iter(seminar))
+        data = seminar[name]
+
+        time = data["time"]
+        times.append({
+            "name": name,
+            "start": int(time[0:2])*60 + int(time[3:5]),
+            "end": int(time[6:8])*60 + int(time[9:11])
+        })
+    times.sort(key=lambda e: e["start"])
+    
+    overlaps = []
+    for i in range(len(times) - 1):
+        this_overlaps = []
+        for j in range(i + 1, len(times) - 1):
+            if times[i]["end"] > times[j]["start"]:
+                this_overlaps.append(times[j]["name"])
+        if len(this_overlaps) > 0:
+            overlaps.append(f"\"{times[i]['name']}\" extends into \"" + "\", \"".join(this_overlaps) + "\"")
+
+    if len(overlaps) > 0:
+        raise Exception("The following seminars overlap:\n" + "\n".join(overlaps))
+
+
 def validate_whats_on(schedule):
     validate_seminars(schedule)
     for seminar in schedule:
@@ -28,7 +56,12 @@ def validate_whats_on(schedule):
             time_match = re.match("\d{2}:\d{2}-\d{2}:\d{2}", time)
             if not time_match:
                 raise Exception(f"\"time\" value for seminar \"{name}\" does not match hh:mm-hh:mm format")
-            # TODO: Validate that the times are valid and the start comes before the end
+            if int(time[0:2]) > 24 or int(time[3:5]) > 59:
+                raise Exception("Start time is not on a regular 24 hour clock")
+            if int(time[6:8]) > 24 or int(time[9:11]) > 59:
+                raise Exception("End time is not on a regular 24 hour clock")
+            if int(time[0:2])*60 + int(time[3:5]) > int(time[6:8])*60 + int(time[9:11]):
+                raise Exception("Start time must come before end time")
         else:
             raise Exception(SEMINAR_MISSING_PROP_EXCEPTION.format(name, "time"))
 
@@ -40,6 +73,7 @@ def validate_whats_on(schedule):
         alias = data.get("alias")
         if alias and len(alias) >= len(name):
             raise Exception(f"The alias \"{alias}\" should be shorter than its seminar name \"{name}\"")
+    validate_seminar_times(schedule)
 
 def validate_date(date):
     try:
