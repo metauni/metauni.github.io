1f26de970445c71c460d1f0407a49d52efdb44ea
phoebe <13684891+phoebethewitch@users.noreply.github.com>
2022-06-29 16:10:44 -0400

    add icalendar

--- index.md
+++ index.md
@@ -24,6 +24,8 @@ We are currently running a free 8-week introductory course on algebraic geometry
 We meet once a week for classes and seminars. To join enter the [metauni Hub](https://www.roblox.com/games/8165000775/metauni-Hub) and click the "Join now" button on the What's On board. All are welcome. Click the name of the seminar for a detailed schedule of future talks and links to videos of past talks.
 
 Times are given on Thursday AEST. Names in italics are the co-organisers of the seminar.
+
+[The schedule is also available in the iCalendar format.](webcal://metauni.org/schedule.ics)
 <!-- BEGIN WHATS ON -->
 * **[metauni Algebraic Geometry](https://metauni.org/mag/)** **07:00-08:30** (*Ken Chan, Dan Murfet*): Lecture 3
 * **Tea break** **08:30-09:30**
---
+++ schedule/update-icalendar.py
@@ -0,0 +1,58 @@
+import os
+import yaml
+from ics import Calendar, Event
+from datetime import datetime
+
+schedule = None
+with open(os.environ["SCHEDULE_PATH"], "r", encoding="utf-8") as f:
+    schedule = yaml.safe_load(f)
+
+DATE = schedule["date"]
+TIMEZONE = schedule["timezone"]
+
+cal = Calendar()
+
+def parse_event_times(time):
+    start_time = time[0:5]
+    end_time = time[6:11]
+    return (
+        datetime.strptime(f"{DATE} {start_time} {TIMEZONE}", "%d/%m/%Y %H:%M %z"),
+        datetime.strptime(f"{DATE} {end_time} {TIMEZONE}", "%d/%m/%Y %H:%M %z")
+    )
+
+def add_seminar_to_calendar(seminar):
+    name = next(iter(seminar))
+    data = seminar[name]
+
+    time = data.get("time")
+    organizer = data.get("organizer")
+    desc = data.get("desc")
+    note = data.get("note")
+    website = data.get("website")
+    location = data.get("location")
+
+    scheduled_start_time, scheduled_end_time = parse_event_times(time)
+
+    event = Event()
+    event.name = name
+    event.begin = scheduled_start_time.isoformat()
+    event.end = scheduled_end_time.isoformat()
+    event.description = ""
+
+    if desc or note:
+        event.description = ""
+        if desc:
+            event.description += desc
+        if note:
+            event.description += "\n\n" + note
+
+    event.location = location
+    event.url = website
+
+    cal.events.add(event)
+
+for seminar in schedule["whats on"]:
+    add_seminar_to_calendar(seminar)
+
+with open('schedule.ics', 'w') as f:
+    f.writelines(cal)
